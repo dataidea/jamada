a73e23057f51f5b9b3c50f8d60ecd25eb0de13e4
jumashafara <jumashafara0@gmail.com>
2023-11-24 18:34:53 +0300

    added pythonanywhere root

--- jamada/settings.py
+++ jamada/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'jamada.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', '/home/professorjamada/jamada/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
